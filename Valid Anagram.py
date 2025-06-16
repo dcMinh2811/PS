86887ecdb6226d8320e4e90f674e0d44d5927fb8
dcMinh2811 <dcminh2811@gmail.com>
2025-06-16 09:18:49 +0700

    Create Valid Anagram.py

---
+++ Valid Anagram.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        s = sorted(s)
+        t = sorted(t)
+
+        if s == t:
+            return True
+        else:
+            return False
