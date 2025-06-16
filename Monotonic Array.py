f8b6ae2ec207f41679362c7181c66afba02a829a
dcMinh2811 <dcminh2811@gmail.com>
2025-06-16 17:59:48 +0700

    Create Monotonic Array.py

---
+++ Monotonic Array.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def isMonotonic(self, nums):
+        if nums == sorted(nums) or nums == sorted(nums, reverse=True):
+            return True
+        else:
+            return False
