df730fbddf85e6d33f02f6b4ed0c16516696f867
dcMinh2811 <dcminh2811@gmail.com>
2025-06-17 09:48:44 +0700

    Update Monotonic Array.py

--- Monotonic Array.py
+++ Monotonic Array.py
@@ -1,6 +1,16 @@
 class Solution(object):
     def isMonotonic(self, nums):
-        if nums == sorted(nums) or nums == sorted(nums, reverse=True):
+        inc = False
+        dec = False
+        for i, n in enumerate(nums):
+            try:
+                if n > nums[i+1]:
+                    dec = True
+                elif n < nums[i+1]:
+                    inc = True
+            except IndexError:
+                pass
+        if not inc == dec == True:
             return True
         else:
             return False
